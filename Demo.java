8798de8222f65ceb5b45bac0bce7a62d040e0a79
aarthiganesan2805 <aarthiganesan2805@gmail.com>
2026-06-09 13:26:11 +0530

    Add Demo class with main method and World function

---
+++ Demo.java
@@ -0,0 +1,17 @@
+class Demo {
+
+ static void World(){
+    System.out.println("Demo project");
+  System.out.println("Hello world");
+  
+
+
+}
+    
+    public static void main(String[] args) {
+        
+
+World(); 
+    }
+    
+}
